3310c247bd0ac241669904e05f9e92c911b03430
pranav-dotnet <goshikepranav@gmail.com>
2026-01-04 18:59:15 -0500

    Create switch-case.cs

---
+++ switch-case.cs
@@ -0,0 +1,22 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int day = 2;
+
+        switch (day)
+        {
+            case 1:
+                Console.WriteLine("Monday");
+                break;
+            case 2:
+                Console.WriteLine("Tuesday");
+                break;
+            default:
+                Console.WriteLine("Invalid day");
+                break;
+        }
+    }
+}
